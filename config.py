6f53a83b09458bf8cd68c3372e1570f312f83e56
a.baranov <yuiomer1337@gmail.com>
2022-09-16 17:51:28 +0300

    1 vers

--- config.py
+++ config.py
@@ -1,1 +1,2 @@
 Bot_token = '5175639035:AAEtjEZq0skngkrA6Nv_QehKCjLb61FKE-g'
+spisok_del =[]
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import telebot
 from config import Bot_token as BT
+from config import spisok_del
 from telebot import types
 
 bot = telebot.TeleBot(BT, parse_mode='None')
@@ -20,21 +21,28 @@ def send_welcome(message):
 	bot.reply_to(message, "Добро пожаловать в общий список дел по домашней бытовухе", reply_markup=markup)
 
 
-A = ['1', '2']
 
-def read_spisok(message):
-	for i in A:
+def read_spisok(message, spisok_del):
+	for i in spisok_del:
 		data_messege = str(i)
 		bot.send_message(message.chat.id, data_messege)
 
 @bot.message_handler(content_types=['text'])
 def main_act(message):
-	if message.chat.type == 'private':
-		bot.send_message(message.chat.id, 'Хорошо сейчас')
-		if message.text == 'Добавить задачу':
-			bot.send_message(message.chat.id, 'Добавляю задачу')
-		elif message.text == 'Посмотреть задачи':
-			read_spisok(message)
+#	if message.chat.type == 'private':
+#		bot.send_message(message.chat.id, 'Хорошо сейчас')
+
+	if message.text == 'Добавить задачу':
+		bot.send_message(message.chat.id, 'Какую задачу Вы хотите добавить ?))')
+
+	elif message.text == 'Посмотреть задачи':
+		read_spisok(message, spisok_del)
+
+
+@bot.message_handler(commands=['Добавить задачу'])
+def main_act(message):
+	new_message = message.text
+	bot.send_message(message.chat.id, f'Вы добавили {new_message}')
 
 
 bot.polling(none_stop=True)
