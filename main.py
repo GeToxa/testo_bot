078125967914d81129bf31b806ee55e84e6d5228
a.baranov <yuiomer1337@gmail.com>
2022-09-19 12:07:05 +0300

    3 vers

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import telebot
 from config import Bot_token as BT
-from config import spisok_del
+from config import spisok_del, help_notification, slovar_del
 from telebot import types
 
 bot = telebot.TeleBot(BT, parse_mode='None')
@@ -18,20 +18,18 @@ def send_welcome(message):
 	markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
 	item1 = types.KeyboardButton('Добавить задачу')
 	item2 = types.KeyboardButton('Посмотреть задачи')
-	markup.add(item1, item2)
+	item3 = types.KeyboardButton('Удалить задачу')
+	markup.add(item1, item2, item3)
 
 	# Ответы
 
-	bot.reply_to(message, f"Добро пожаловать {user_name}. Этот бот составляет общий список дел. Теперь не увернуться от дел!",
+	bot.reply_to(message, f"Добро пожаловать {user_name}. Этот бот составляет общий список дел. Теперь все записано",
 				 reply_markup=markup)
 
-	bot.send_message(message, "Для вызова справки напишите слово 'справка'")
+	bot.send_message(message, "Для вызова справки напишите слово 'Cправка'")
 
 
-def read_spisok(message, spisok_del):
-	for i in spisok_del:
-		data_messege = str(i)
-		bot.send_message(message.chat.id, data_messege)
+
 
 @bot.message_handler(content_types=['text'])
 def main_act(message):
@@ -42,17 +40,57 @@ def main_act(message):
 
 	if message.text == 'Добавить задачу':
 		add_choise = bot.send_message(message.chat.id, 'Какую задачу Вы хотите добавить ?))')
-
 		bot.register_next_step_handler(add_choise, new_msg_handler_add)
 
 	if message.text == 'Посмотреть задачи':
 		read_spisok(message, spisok_del)
 
+	if message.text == 'Cправка':
+		bot.send_message(message.chat.id, help_notification)
+
+	if message.text == 'Удалить задачу':
+
+		del_choise = bot.send_message(message.chat.id, 'Напишите номер задачи которую хотите удалить)')
+		bot.register_next_step_handler(del_choise, chose_to_del)
+
+def chose_to_del(message):
+	try:
+		del_number = message.text
+		del_number = int(del_number)
+		del_number = del_number - 1
+		element = slovar_del[del_number]
+		spisok_del.remove(element)
+	except ValueError:
+		bot.send_message(message.chat.id, 'Вы ввели не число')
+	else:
+		bot.send_message(message.chat.id, 'Задача удалена')
+
+
 def new_msg_handler_add(message):
 
 	new_message_add = message.text
 	spisok_del.append(new_message_add)
-	bot.send_message(message.chat.id, f'Вы добавили: {new_message_add}')
+
+	last_task = chek_len()
+
+	last_task = last_task[-1]
+	bot.send_message(message.chat.id, f'Вы добавили задачу {last_task}:  {new_message_add}')
+
+	for i in range(len(spisok_del)):
+		slovar_del[i] = spisok_del[i]
+
+def chek_len():
+	all_tasks = len(spisok_del)
+	np_spis_add = []
+	for i in range(all_tasks):
+		np_spis_add.append(i+1)
+	return np_spis_add
+
+def read_spisok(message, spisok_del):
+
+	for i in range(len(spisok_del)):
+		stroka = f'{i+1}: {slovar_del[i]}'
+		bot.send_message(message.chat.id, stroka)
 
 
 bot.polling(none_stop=True)
